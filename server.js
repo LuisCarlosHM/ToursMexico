9676975f7baa3f7d2be75cdbba5af95127caafd5
Luipoleon <luis.luigis.9999@gmail.com>
2024-10-07 13:09:50 -0600

    Update main page

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ dotenv.config({ path: './config.env' });
 
 const app = require('./app');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.PASSWORD);
 
 mongoose.set('strictQuery', false);
@@ -15,6 +15,6 @@ mongoose
   .then((con) => console.log('DB connection successful!'));
 
 
-const server = app.listen(3000, () => {
+const server = app.listen(port, () => {
     console.log(`Express is running on port ${server.address().port}`);
 });
