4cd5be5042619a22792c997330cbdbd9678b3532
Luipoleon <luis.luigis.9999@gmail.com>
2024-10-18 13:06:20 -0600

    Register page working

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,7 @@
 const preload = document.querySelector('.preload');
 const loginForm = document.querySelector('.login__form');
 const btnCloseSession = document.querySelector('#btn-close-session');
+const registerForm = document.querySelector('.register__form');
 
 document.addEventListener('DOMContentLoaded', function() {
     setTimeout(function() {
@@ -42,6 +43,38 @@ if(loginForm){
         });
 
         if (response.status == 200){
+            window.location.href = '/';   
+            return;
+        }
+
+        alert('Credenciales incorrectas');
+
+    });
+}
+
+if(registerForm){
+    registerForm.addEventListener('submit', async function(e) {
+        e.preventDefault();
+
+        const name = document.querySelector('#first_name').value + ' ' + document.querySelector('#last_name').value;
+        const email = document.querySelector('#email').value;
+        const password = document.querySelector('#password').value;
+        const passwordConfirm = document.querySelector('#password_confirm').value;
+
+        if(password !== passwordConfirm){
+            alert('Las contraseñas no coinciden');
+            return;
+        }
+        
+        const response = await fetch('/api/v1/users/signup', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({name, email, password, passwordConfirm})
+        });
+        console.log(response);
+        if (response.status == 201){
             window.location.href = '/';   
         }
     });
